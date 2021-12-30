7273cd5b5df322e353aa43fcefcf737a165e2d32
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2021-12-30 13:07:30 +0900

    Upload 211230

--- jsp_project/OpenProject/src/jdbc/ConnectionProvider.java
+++ jsp_project/OpenProject/src/jdbc/ConnectionProvider.java
@@ -5,11 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
+
 	// Connection 객체를 제공하는 메소드 : public static
 	public static Connection getConnection() throws SQLException {
 		
 		String url = "jdbc:mysql://localhost:3306/project?serverTimezone=UTC";
 		
 		return DriverManager.getConnection(url, "bit", "bit");
+		
 	}
+	
 }
