956a50e1400b0f0d7820e05587633177648d61bb
Seongmo Gu <77833389+gusm96@users.noreply.github.com>
2022-01-12 18:25:57 +0900

    Upload 220112

--- spring_project/OpenProject/src/main/java/com/bitcamp/op/member/service/CommonsData.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/service/CommonsData.java
@@ -2,5 +2,7 @@ package com.bitcamp.op.member.service;
 
 public interface CommonsData {
 	// 파일을 저장할 URI
+	// 상속으로 만들어 두는 이유는
+	// 외부로 부터 영향을 받지 않도록 하기 위해.
 	String SAVE_URI = "/uploadfile";
 }
---
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberListService.java
@@ -0,0 +1,7 @@
+package com.bitcamp.op.member.service;
+
+public class MemberListService {
+	public Object gerParamView(int pageNum) {
+		return null;
+	}
+}
